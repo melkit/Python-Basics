833e49221bae8ae44fbfcbc19fa33631d6930533
melkit <31418776+melkit@users.noreply.github.com>
2017-10-01 15:22:30 -0700

    Python Basics

---
+++ Day1_excer1.py
@@ -0,0 +1,20 @@
+print ("Hello World")
+print ("Good Morning world")
+print ("I Love My life")
+name = raw_input ("What is your name? : " )
+print ("Hello", name)
+address = raw_input ("Where do you live? : ")
+print ( "You live in " + address)
+
+name= raw_input ("enter name")
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print " Hi %r. So, you're %r yrs old, %r  feet tall and %r pound heavy." % (name,age, height, weight)
+
+
+color = raw_input ("What color do you like? ")
+number = raw_input ("what number do you like? ")
+animal = raw_input ("which animal do you like? ")
+print "So, you have %r numbers of  %r and they are %r in color." % (number, animal, color)
