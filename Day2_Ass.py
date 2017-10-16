5f644f8528d6391bbd76403cab54a60dec6b2a87
melkit <31418776+melkit@users.noreply.github.com>
2017-10-16 13:49:27 -0700

    Add files via upload

---
+++ Day2_Ass.py
@@ -0,0 +1,7 @@
+number1=(int)(input("How many cookies did John eat?: "))
+number2=(int)(input("How many breads did John eat?: "))             
+
+answer=number1+number2
+
+print ("The answer = " + str(answer))
+print ("John ate %r in total.") % (answer)
